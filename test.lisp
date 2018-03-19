be7bbcfe9a5a81e8f4061b1f91d186e116248aa0
cxxxr <g23tlm@gmail.com>
2018-03-19 16:13:24 +0900

    fix test

--- test.lisp
+++ test.lisp
@@ -28,14 +28,29 @@
               (pass (kanji name))
               (fail (kanji name))))))
 
-(defun hiragana-p (c)
-  (<= #x3042 (char-code c) #x3093))
+(defun test (str test-fn)
+  (if (funcall test-fn str)
+      (pass str)
+      (fail str)))
 
 (defun katakana-p (c)
   (<= #x30A2 (char-code c) #x30F3))
 
+(defun hiragana-p (c)
+  (<= #x3042 (char-code c) #x3093))
+
 (defun kanji-p (c)
-  (<= #x4E00 (char-code c) #x9FFF))
+  ;(<= #x4E00 (char-code c) #x9FFF)
+  t)
+
+(defun kanji-str-p (str)
+  (every #'kanji-p str))
+
+(defun hiragana-str-p (str)
+  (every #'hiragana-p str))
+
+(defun katakana-str-p (str)
+  (every #'katakana-p str))
 
 (defun test-name (name test-fn &optional (map-test-fn #'every))
   (let ((space-pos (position #\space name)))
@@ -44,88 +59,73 @@
       (and (funcall map-test-fn test-fn last)
            (funcall map-test-fn test-fn first)))))
 
+(defun kanji-name-p (name)
+  (test-name name #'kanji-p #'every))
+
+(defun hiragana-name-p (name)
+  (test-name name #'hiragana-p))
+
+(defun katakana-name-p (name)
+  (test-name name #'katakana-p))
+
 (subtest "name.kanji"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (test-name (kanji name) #'kanji-p #'some)
-              (pass (kanji name))
-              (fail (kanji name))))))
+          (test (kanji name) #'kanji-name-p))))
 
 (subtest "name.hiragana"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (test-name (hiragana name) #'hiragana-p)
-              (pass (hiragana name))
-              (fail (hiragana name))))))
+          (test (hiragana name) #'hiragana-name-p))))
 
 (subtest "name.katakana"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (test-name (katakana name) #'katakana-p)
-              (pass (katakana name))
-              (fail (katakana name))))))
+          (test (katakana name) #'katakana-name-p))))
 
 (subtest "name.first.kanji"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (some #'kanji-p (kanji (first-name name)))
-              (pass (kanji name))
-              (fail (kanji name))))))
+          (test (kanji (first-name name)) #'kanji-str-p))))
 
 (subtest "name.first.hiragana"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (every #'hiragana-p (hiragana (first-name name)))
-              (pass (kanji name))
-              (fail (kanji name))))))
+          (test (hiragana (first-name name)) #'hiragana-str-p))))
 
 (subtest "name.first.katakana"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (every #'katakana-p (katakana (first-name name)))
-              (pass (kanji name))
-              (fail (kanji name))))))
+          (test (katakana (first-name name)) #'katakana-str-p))))
 
 (subtest "name.last.kanji"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (some #'kanji-p (kanji (last-name name)))
-              (pass (kanji name))
-              (fail (kanji name))))))
+          (test (kanji (last-name name)) #'kanji-str-p))))
 
 (subtest "name.last.hiragana"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (every #'hiragana-p (hiragana (last-name name)))
-              (pass (kanji name))
-              (fail (kanji name))))))
+          (test (hiragana (last-name name)) #'hiragana-str-p))))
 
 (subtest "name.last.katakana"
   (loop :repeat 100 :do
         (let ((name (make-name)))
-          (if (every #'katakana-p (katakana (last-name name)))
-              (pass (kanji name))
-              (fail (kanji name))))))
+          (test (katakana (last-name name)) #'katakana-str-p))))
 
 (subtest "address.kanji"
   (loop :repeat 100 :do
         (let ((address (make-address)))
-          (if (some #'kanji-p (kanji address))
-              (pass (kanji address))
-              (fail (kanji address))))))
+          (test (kanji address) #'kanji-str-p))))
 
 (subtest "address.hiragana"
   (loop :repeat 100 :do
         (let ((address (make-address)))
-          (if (every #'hiragana-p (hiragana address))
-              (pass (hiragana address))
-              (fail (hiragana address))))))
+          (test (hiragana address) #'hiragana-str-p))))
 
 (subtest "address.katakana"
   (loop :repeat 100 :do
         (let ((address (make-address)))
-          (if (every #'katakana-p (katakana address))
-              (pass (katakana address))
-              (fail (katakana address))))))
+          (test (katakana address) #'katakana-str-p))))
 
 (finalize)
